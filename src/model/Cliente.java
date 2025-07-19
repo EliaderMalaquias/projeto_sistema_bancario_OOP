4164932ba96423c28c06a64549174b8b624a393b
Eliader Malaquias <joseeliader190@gmail.com>
2025-07-18 23:00:12 -0300

    Criando Entidade do Sistema

---
+++ src/model/Cliente.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String senha;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String cpf, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+}
---
+++ src/model/Conta.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Conta {
+
+    private int numero;
+    private double saldo;
+    private Cliente titular;
+
+    public Conta() {
+    }
+
+    public Conta(int numero, Cliente titular) {
+        this.numero = numero;
+        this.saldo = 0.0;
+        this.titular = titular;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+}
