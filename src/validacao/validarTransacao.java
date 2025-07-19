056445c15eac4655f6af775d5a8b99f50336bf9a
Eliader Malaquias <joseeliader190@gmail.com>
2025-07-18 23:23:55 -0300

    Adicionando Funcionalidades na validacoes

--- src/validacao/validarTransacao.java
+++ src/validacao/validarTransacao.java
@@ -1,4 +1,26 @@
 package validacao;
 
 public class validarTransacao {
+
+    public static void validarDeposito(double valor) {
+        if(valor <= 0) {
+            System.out.println("Operação Negada: O Valor não pode ser Igual ou Menor que 0!");
+        }
+    }
+
+    public static void validarSaque(double valor,  double saldo) {
+        if (valor <= 0) {
+            System.out.println("Operação Negada: O Valor não pode ser Igual ou Menor que 0!");
+        } else if (valor > saldo) {
+            System.out.println("Operação Negada: Saldo Insulficiente!");
+        }
+    }
+
+    public static void validarTransferencia(double valor,  double saldo) {
+        if (valor <= 0) {
+            System.out.println("Operação Negada: O Valor não pode ser Igual ou Menor que 0!");
+        } else if (valor > saldo) {
+            System.out.println("Operação Negada: Saldo Insulficiente!");
+        }
+    }
 }
