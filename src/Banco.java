00a69a0f459a7a527bb461fedb94fec6fc86d657
Eliader Malaquias <joseeliader190@gmail.com>
2025-07-19 00:34:39 -0300

    Adicionando Funcionalidades no Sitema

--- src/Banco.java
+++ src/Banco.java
@@ -6,4 +6,27 @@ import java.util.List;
 public class Banco {
 
     List<Cliente> clientesDoSistema = new ArrayList<>();
+
+    public void criarCliente(Cliente cliente) {
+        clientesDoSistema.add(cliente);
+    }
+
+    public Cliente getClientePorCPF(String cpf) {
+        Cliente cliente = null;
+
+        for (Cliente getCliente : clientesDoSistema) {
+            if (cpf.equals(getCliente.getCpf())) {
+                cliente = getCliente;
+            }
+
+        }
+
+        return cliente;
+    }
+
+    public void exibirSaldo(String cpf) {
+        Cliente cliente = getClientePorCPF(cpf);
+
+        System.out.println("Saldo da Conta: R$");
+    }
 }
